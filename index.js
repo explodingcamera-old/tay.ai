6a0ffbf4d6bd568d1c1a82137ca61fcadfefef3e
henr-y <mail@explodingcamera.com>
2016-03-25 15:07:22 +0100

    Progress!

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Promise = require('bluebird');
 const twitter = require('twitter');
+const EventEmitter = require('events').EventEmitter;
+const events = new EventEmitter();
+events.emit = Promise.promisify(events.emit);
 
 function tay(args) {
   var _this = this;
@@ -17,20 +20,46 @@ function tay(args) {
       access_token_secret: _this.twitterKeys.accessTokenSecret,
     });
     return new Promise(function (resolve) {
-      console.log(_this.TwitterClient);
-      _this.TwitterClient.get('application/rate_limit_status', function (error, data, response) {
-        if (error) {
-          reject(error);
-          return;
-        }
+      _this.TwitterClient.stream('user', function (stream) {
+        _this.TwitterClient.get('application/rate_limit_status', function (error, data, response) {
+          if (error) {
+            reject(error);
+            return;
+          }
 
-        resolve();
+          resolve();
+        });
+
+        stream.on('data', function (data) {
+          if (data.direct_message && data.direct_message.sender.id === 4531940473) {
+            events.emit('answer', data.direct_message.text)
+            .then(function (data) {
+              console.log(data);
+            });
+          } else {
+            events.emit('answer', 'An error acurred');
+          }
+        });
+
+        stream.on('error', function (error) {
+          console.log(error);
+        });
+
+        stream.on('disconnect', function (disconnectMessage) {
+          stream.start();
+        });
       });
     });
   };
 
-  this.ask = function (question) {
+  this.send = function (question) {
+    return new Promise(function (resolve) {
+      _this.TwitterClient; //TODO
+    });
+  };
 
+  this.onAnswer = function (cb) {
+    return events.on('answer', cb);
   };
 }
 
